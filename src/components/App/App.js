8ec9aa24ea863fa82233cc9131093b966772762c
Fernando Poumián <fpoumian@gmail.com>
2018-03-23 13:58:55 -0700

    Create components dir and create custom App component

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import styles from './App.module.css'
+
+const App = () => (
+  <div>
+    <h1 className={styles.title}>{`Heading`}</h1>
+  </div>
+)
+
+export default App
